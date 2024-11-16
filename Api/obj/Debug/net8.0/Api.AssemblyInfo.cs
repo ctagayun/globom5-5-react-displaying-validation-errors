5c03c3ece55ae487705263dd010fcd62edabf017
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-11-16 16:23:18 -0600

    Completed

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a50f1c8b78c8cc1bd1ddef0edf1b3ec1236a6b97")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
